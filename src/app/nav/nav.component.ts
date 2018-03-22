1770abd15ac89263594d462f8f488e43cea89a85
Eric Lavagni <LISSABE@capitol.local>
2018-03-21 19:54:51 -0500

    Change loggedIn validation

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -31,6 +31,7 @@ export class NavComponent implements OnInit {
 
     loggedIn() {
       const token = localStorage.getItem('token');
-      return !token;
+      console.log(token);
+      return token;
     }
   }
