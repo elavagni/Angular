bf81efaec1df7bf570105895072a05ca80e3fc7f
eric <elavagnim@gmail.com>
2018-03-24 17:28:41 -0500

    Clean up testing code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,18 +16,10 @@ export class HomeComponent implements OnInit {
    }
 
   ngOnInit() {
-    this.getValues();
   }
 
   registerToggle() {
     this.registerMode = true;
-  }
-
-  getValues() {
-    this.http.get('http://localhost:5000/api/values').subscribe(
-      response => {
-        this.values = response.json();
-      });
   }
 
   cancelRegisterMode( registerMode: boolean) {
