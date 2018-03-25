126c3994f87e93f4a442e48fe483ae2726352f35
eric <elavagnim@gmail.com>
2018-03-25 18:45:03 -0500

    Added routing

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -11,7 +12,9 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authservice: AuthService, private alertify: AlertifyService) {}
+  constructor(public authservice: AuthService, 
+              private alertify: AlertifyService,
+              private router: Router) {}
 
   ngOnInit() {
   }
@@ -21,6 +24,8 @@ export class NavComponent implements OnInit {
      this.alertify.success('logged in sucessfully');
     }, error => {
       this.alertify.error('Failed to login');
+    }, () => {
+        this.router.navigate(['/members']);
     });
   }
 
@@ -28,6 +33,7 @@ export class NavComponent implements OnInit {
       this.authservice.userToken = null;
       localStorage.removeItem('token');
       this.alertify.message('log out');
+      this.router.navigate(['/home']);
     }
 
     loggedIn() {
