19e6af0195fbc3a32ae251ea56cab39097d2e240
eric <elavagnim@gmail.com>
2018-03-25 17:48:59 -0500

    improved login features, added look and feel with bootstrap, and ui notifications

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './_services/auth.service';
+import { JwtHelper } from 'angular2-jwt';
+import { Token } from '@angular/compiler';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  jwtHelper = new JwtHelper();
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      this.authService.decotedToken = this.jwtHelper.decodeToken(token);
+    }
+  }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -10,27 +11,26 @@ export class NavComponent implements OnInit {
 
   model: any = {};
 
-  constructor(private authservice: AuthService){}
+  constructor(private authservice: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {
   }
 
   login()  {
     this.authservice.login(this.model).subscribe(data => {
-      console.log('logged in sucessfully');
+     this.alertify.success('logged in sucessfully');
     }, error => {
-      console.log('Failed to login');
+      this.alertify.error('Failed to login');
     });
   }
 
     logout() {
       this.authservice.userToken = null;
       localStorage.removeItem('token');
-      console.log('log out');
+      this.alertify.message('log out');
     }
 
     loggedIn() {
-      const token = localStorage.getItem('token');
-      return !!token;
+      return this.authservice.loggedIn();
     }
   }
