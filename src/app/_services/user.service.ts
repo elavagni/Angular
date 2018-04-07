36f25225ceef09ba01d59624c54446f6229f8c66
eric <elavagnim@gmail.com>
2018-04-07 17:36:55 -0500

    Changes to upload photos

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -29,6 +29,10 @@ updateUser(id: number, user: User) {
     return this.authHttp.put(this.baseUrl + 'users/' + id, user).catch(this.handleError);
 }
 
+setMainPhoto(userId: number, id: number) {
+    return this.authHttp.post(this.baseUrl + 'users/' + userId + '/photos/' + id + '/setMain', {}).catch(this.handleError);
+}
+
 private handleError(error: any) {
     const applicationError = error.headers.get('Application-Error');
     if (applicationError) {
